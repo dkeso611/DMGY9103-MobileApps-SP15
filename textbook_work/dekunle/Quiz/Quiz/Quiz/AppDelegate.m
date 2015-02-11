8dc916d7f69c6ed83e37b74d09068d4d3c93a2f1
dkeso611 <dko208@nyu.edu>
2015-02-11 18:39:53 -0500

    Quiz
    Quiz app

--- textbook_work/dekunle/Quiz/Quiz/Quiz/AppDelegate.m
+++ textbook_work/dekunle/Quiz/Quiz/Quiz/AppDelegate.m
@@ -3,7 +3,7 @@
 //  Quiz
 //
 //  Created by Kes on 2/10/15.
-//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//  Copyright (c) 2015 DK Media. All rights reserved.
 //
 
 #import "AppDelegate.h"
